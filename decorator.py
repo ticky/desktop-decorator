3df8f2289bc0f01cd64a718f94aeed15afdb6477
Geoff Stokes <geoff@geoffstokes.net>
2014-07-09 20:37:07 +1000

    Python 3 compatibility, accept files as arguments

--- decorator.py
+++ decorator.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# coding: utf-8
+#!/usr/bin/env python
 
 # Desktop Decorator
 
@@ -7,36 +6,37 @@ from fractions import Fraction
 from math import fabs
 from numpy import asarray
 from PIL import Image, ImageDraw, ImageFilter, ImageOps
-from Tkinter import Tk
 import os
+from sys import version as pythonver
 
-VERBOSE	= False
+if pythonver > '3':
+	from tkinter import Tk
+else:
+	from Tkinter import Tk
 
 def main():
 
 	import argparse
 
-	parser = argparse.ArgumentParser(description='Generate display-optimised wallpaper images from a directory of wallpapers.', epilog='Which is how you do it.')
+	parser = argparse.ArgumentParser(description='Optimise wallpaper images for a particular screen resolution.', epilog='Which is how you do it.')
 
 	cli_required = parser.add_argument_group('Required Arguments')
-	cli_required.add_argument('source', metavar='source', type=str, help='directory to load images from')
+	cli_required.add_argument('image', metavar='image', type=str, nargs='+', help='source image file')
 
 	cli_output = parser.add_argument_group('Output Options')
 	cli_output.add_argument('-x', '--width', metavar='width', type=int, help='change desired width of wallpapers (default: detect desktop resolution)')
 	cli_output.add_argument('-y', '--height', metavar='height', type=int, help='change desired height of wallpapers (default: detect desktop resolution)')
-	cli_output.add_argument('-u', '--upscale', action='store_const', const=True, default=False, help='allow images to be scaled up, rather than only down')
+	cli_output.add_argument('-u', '--upscale', action='store_true', help='allow images to be scaled up, rather than only down')
 	cli_output.add_argument('-o', '--output', metavar='output', type=str, default='output-images', help='allow images to be scaled up, rather than only down')
 
 	cli_iproc = parser.add_argument_group('Image Processing Options')
-	cli_iproc.add_argument('-m', '--mask', action='store_const', const=True, default=False, help='enable centre masking (forces feature detection to focus on edges)')
-	cli_iproc.add_argument('-g', '--gradient-mask', action='store_const', const=True, default=False, help='use gradient centre masking (requires -m)')
+	cli_iproc.add_argument('-m', '--mask', action='store_true', help='enable centre masking (forces feature detection to focus on edges)')
+	cli_iproc.add_argument('-g', '--gradient-mask', action='store_true', help='use gradient centre masking (requires -m)')
 	cli_iproc.add_argument('-t', '--threshold', metavar='threshold', type=int, default=166, choices=range(256), help='change the threshold for feature detection, must be between 0 and 255 (default: 166)')
 
 	cli_other = parser.add_argument_group('Other Options')
-	cli_other.add_argument('-v', '--verbose', action='store_const', const=True, default=False, help='show verbose (debug) output')
+	cli_other.add_argument('-v', '--verbose', action='store_true', help='show verbose (debug) output')
 	cli_args = parser.parse_args()
-
-	VERBOSE = cli_args.verbose
 
 	# Detect desktop resolution via Tk if we don't have them specified
 	if cli_args.width == None or cli_args.height == None:
@@ -52,68 +52,77 @@ def main():
 
 		os.makedirs(cli_args.output)
 
-	print 'Desktop Decorator'
-	print '================='
-	print 'Attempting to optimise wallpapers for a display at %sw @ %s...' % (cli_args.width, ratio_string(target_ratio))
+	print('Desktop Decorator')
+	print('=================')
+	print('Attempting to optimise wallpapers for a display at %sw @ %s...' % (cli_args.width, ratio_string(target_ratio)))
 
-	for (dirpath, dirnames, filenames) in os.walk(cli_args.source):
+	for filename in cli_args.image:
 
-		for name in filenames:
+		try:
 
-			if os.path.splitext(name)[-1][1:] in ['jpg', 'jpeg', 'png']:
+			image = Image.open(filename)
 
-				print '* Processing "%s"...' % os.path.splitext(name)[0]
+		except IOError:
 
-				image = Image.open(os.path.join(dirpath, name))
-				image = smart_crop(image, cli_args.width, cli_args.height, cli_args.upscale, cli_args.threshold, cli_args.mask, cli_args.gradient_mask)
+			image = None
 
-				image.save(os.path.join(cli_args.output, '%s.%s' % (os.path.splitext(name)[0], os.path.splitext(name)[-1][1:])))
+			print('* "%s" is being ignored...' % os.path.splitext(os.path.basename(filename))[0])
 
-			else:
+		if type(image) != type(None):
 
-				print '* "%s" is being ignored...' % os.path.splitext(name)[0]
+			print('* Processing "%s"...' % os.path.basename(filename))
 
-	print 'Processing Complete.'
+			image = smart_crop(image, cli_args.width, cli_args.height, cli_args.upscale, cli_args.threshold, cli_args.mask, cli_args.gradient_mask, cli_args.verbose)
+
+			image.save(os.path.join(cli_args.output, '%s-%dw@%s.%s' % (os.path.splitext(os.path.basename(filename))[0], cli_args.width, ratio_string(target_ratio), os.path.splitext(os.path.basename(filename))[-1][1:])))
+
+	print('Processing Complete.')
 
 # TODO: Move smart_crop and find_image_centroid into a library
 def smart_crop(target_image, target_width, target_height, allow_upscale=False, colour_threshold=166,
-				use_mask=False, use_mask_gradient=False):
+				use_mask=False, use_mask_gradient=False, verbose=False):
 
 	# Detect target aspect ratio, and current image's aspect ratio
 	target_aspect_ratio = Fraction(target_width, target_height)
 	existing_aspect_ratio = Fraction(target_image.size[0], target_image.size[1])
 
 	# Debug log of aspect ratio and width
-	log('%sw @ %s' % (target_image.size[0], ratio_string(existing_aspect_ratio)))
+	if verbose:
+		print('%sw @ %s' % (target_image.size[0], ratio_string(existing_aspect_ratio)))
 
 	# If the aspect ratios match, we can get away with just scaling
 	if target_aspect_ratio == existing_aspect_ratio:
 
-		log('Image does not need cropping...')
+		if verbose:
+			print('Image does not need cropping...')
 
 		# If the image is the same size as we want, we can just output it as-is
 		if target_image.size[0] == target_width and target_image.size[1] == target_height:
 
-			log('Image is correct size...')
+			if verbose:
+				print('Image is correct size...')
 
 		# Otherwise, if we're allowed to upscale it, or if it's bigger than we need, scale it using
 		# the best algorithm we have access to
 		elif allow_upscale == True or target_image.size[0] > target_width and target_image.size[1] > target_height:
 
-			log('Image being resized...')
+			if verbose:
+				print('Image being resized...')
 			target_image = target_image.resize((target_width, target_height), Image.ANTIALIAS)
 
 		# Smaller image, but told not to upscale
 		else:
 
-			log('Image accepted as-is...')
+			if verbose:
+				print('Image accepted as-is...')
 
 		return target_image
 
 	# If we need to update the image
 	else:
 
-		log('Image sizes differ by %s pixels horizontally, and %s pixels vertically.' % (int(fabs(target_image.size[0] - target_width)), int(fabs(target_image.size[1] - target_height))))
+		if verbose:
+			print('Image sizes differ by %s pixels horizontally, and %s pixels vertically.' % (int(fabs(target_image.size[0] - target_width)), int(fabs(target_image.size[1] - target_height))))
 
 		resize_width = target_width
 		resize_height = target_height
@@ -125,12 +134,14 @@ def smart_crop(target_image, target_width, target_height, allow_upscale=False, c
 		else:
 			resize_width = int(float(target_image.size[0]) * resize_ratio_height)
 
-		log('Resizing to %sx%s (%s)...' % (resize_width, resize_height, ratio_string(Fraction(resize_width, resize_height))))
+		if verbose:
+			print('Resizing to %sx%s (%s)...' % (resize_width, resize_height, ratio_string(Fraction(resize_width, resize_height))))
 		target_image = target_image.resize([resize_width, resize_height], Image.ANTIALIAS)
 
 		image_centroid = find_image_centroid(target_image, colour_threshold, use_mask, use_mask_gradient)
 
-		log('Centroid detected at %s, %s.' % (image_centroid[0], image_centroid[1]))
+		if verbose:
+			print('Centroid detected at %s, %s.' % (image_centroid[0], image_centroid[1]))
 
 		# Best crop based on detected centroid
 		optimal_crop = [
@@ -140,7 +151,8 @@ def smart_crop(target_image, target_width, target_height, allow_upscale=False, c
 			int(image_centroid[1] + target_height / 2)	# Bottom
 		]
 
-		log(optimal_crop)
+		if verbose:
+			print(optimal_crop)
 
 		# Offsets for crop - if the crop is out of bounds, we adjust it here
 		optimal_crop_offset_x = 0
@@ -161,15 +173,17 @@ def smart_crop(target_image, target_width, target_height, allow_upscale=False, c
 		optimal_crop[2] = optimal_crop[2] + optimal_crop_offset_x
 		optimal_crop[3] = optimal_crop[3] + optimal_crop_offset_y
 
-		log(optimal_crop)
+		if verbose:
+			print(optimal_crop)
 
 		# Crop the target image to the dimensions we specified
 		target_image = target_image.crop(optimal_crop)
 		return target_image
 
-def find_image_centroid(target_image, colour_threshold=166, use_mask=False, use_mask_gradient=False):
+def find_image_centroid(target_image, colour_threshold=166, use_mask=False, use_mask_gradient=False, verbose=False):
 
-	log('Converting image to greyscale, and detecting edges...')
+	if verbose:
+		print('Converting image to greyscale, and detecting edges...')
 	edge_detected_image = ImageOps.invert(target_image.copy().filter(ImageFilter.FIND_EDGES))
 
 	# If we're using a mask (to try to direct detection to the edges of the image)
@@ -204,7 +218,8 @@ def find_image_centroid(target_image, colour_threshold=166, use_mask=False, use_
 	edge_detected_image = ImageOps.invert(edge_detected_image).convert("L").point(lambda i: i > colour_threshold and 255 or 0)
 
 	# Centroid logic contributed by Ben Stewart
-	log('Finding centroid...')
+	if verbose:
+		print('Finding centroid...')
 
 	# Sum of colours in image
 	image_sum = sum([sum(row) for row in asarray(edge_detected_image)])
@@ -219,11 +234,6 @@ def find_image_centroid(target_image, colour_threshold=166, use_mask=False, use_
 
 def ratio_string(ratio):
 	return str(ratio).replace('/', 'x')
-
-def log(message):
-	if VERBOSE == True:
-		print '* [DEBUG]', message
-
 
 if __name__ == "__main__":
 	main()
